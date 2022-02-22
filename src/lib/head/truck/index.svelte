46fa9373ea52e6661edcda7f5c23f0cab5f3f657
Isaac Dunnett <is560393@dal.ca>
2022-02-22 00:14:44 -0400

    removed red backround on truck

--- src/lib/head/truck/index.svelte
+++ src/lib/head/truck/index.svelte
@@ -11,5 +11,5 @@
 <div
   id="scene"
   bind:this={sceneDiv}
-  class="w-full lg:w-2/3 max-h-[500px] md:max-h-[750px] xl:max-h-screen h-full tall:bg-[bottom_-2rem_left_-20rem] bg-[bottom_-8rem_left_-20rem] bg-red-500 pointer-events-none lg:pointer-events-auto"
+  class="w-full lg:w-2/3 max-h-[500px] md:max-h-[750px] xl:max-h-screen h-full tall:bg-[bottom_-2rem_left_-20rem] bg-[bottom_-8rem_left_-20rem] pointer-events-none lg:pointer-events-auto"
 />
